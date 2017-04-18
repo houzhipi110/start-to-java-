c44397247171d71c1d764862f1008f72cc4bec6e
houzhipi110 <494024469@qq.com>
2017-04-18 17:34:57 +0800

    List

---
+++ Demo10.java
@@ -0,0 +1,34 @@
+package cn.itcast.list;
+
+import java.util.LinkedList;
+/*
+ 需求： 使用LinkedList存储一副扑克牌，然后实现洗牌功能。
+ 
+ 	52张
+ 	
+ 	一张扑克牌： 花色  点数。  梅花6
+ 
+class Poker{
+	
+	String color;
+	
+	String num;
+} 
+
+String[] colors = {"黑桃","红桃","梅花","方块"};
+
+String[] nums = {"A","2"} 
+ 
+ */
+
+
+public class Demo10 {
+
+	public static void main(String[] args) {
+		LinkedList pokers = new LinkedList();
+	
+		
+		
+		
+	}
+}
